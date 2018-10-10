5d7b30dd5cd938d048d2bcc11ab756308acb1039
bittukumarray <37700371+bittukumarray@users.noreply.github.com>
2018-10-10 23:04:27 +0530

    Update add_even_num.py
    Changed the method of adding even no, it would be faster than the previous one.

--- add_even_num.py
+++ add_even_num.py
@@ -1,10 +1,11 @@
 # This program adds even numbers upto the number specified by the user
 
-n = int(input("Enter the nth term: "))
+n = int(input("Enter the number "))
 
 sum = 0
-for i in range(n):
-  if i%2 == 0:
-    sum = sum + i
+i=0
+while i<=n:
+  sum = sum + i
+  i=i+2
 
 print("Sum is",sum)
